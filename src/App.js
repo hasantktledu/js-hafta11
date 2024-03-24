e2ae8464f8b886b6745e925aa67621c3394abe62
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-03-24 18:41:52 +0300

    useref1 örneği

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useRef, useState } from "react";
 
 function App() {
+  const [sayac, sayacGuncelle] = useState(0)
+
+  const puan = useRef(0)
+
+  function puanGuncelle() {
+    puan.current += 10
+    alert(puan.current)
+  }
+
   return (
     <div>
-      App
+      Puan: {puan.current}
+
+      <div>
+        {sayac}
+        <button onClick={ ()=>{ sayacGuncelle(eskiDeger=>eskiDeger+5) } }>Sayaç Arttır</button>
+      </div>
+      <div>
+        <button onClick={puanGuncelle}>Ref Değeri Güncelle</button>
+      </div>
     </div>
   );
 }
