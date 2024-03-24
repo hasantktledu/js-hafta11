3646030d6bad60c2dc3df64ed418f5533bb34bc6
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-03-24 18:49:57 +0300

    useref2 örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, { useState, useRef, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [sayi, setSayi] = useState(0);
+  const [can, setCan] = useState(3);
+
+  const sayiRef = useRef(0);
+
+  useEffect(() => {
+    sayiRef.current++
+
+    console.log("Sayi değişti:", sayi);
+    console.log("SayiRef değeri:", sayiRef.current-1);
+  }, [sayi]);
+
   return (
     <div>
-      App
+      <p>Sayi: {sayi} | Can: {can}</p>
+      <button onClick={() => setSayi(eskiDeger=>eskiDeger+5)}>Sayıyı Arttır(+5)</button>
+      <button onClick={() => setSayi(eskiDeger=>eskiDeger-3)}>Sayıyı Arttır(-3)</button>
+
+      <button onClick={() => setCan(eskiDeger=>eskiDeger-1)}>Canı Azalt(-1)</button>
     </div>
   );
-}
+};
 
 export default App;
