8ee40715f4756eba5556c98f4eef09cb78e8904a
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-03-24 19:23:57 +0300

    prop değeri saklama örneği

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React, { useEffect, useRef, useState } from "react";
 
-function App() {
+const App = ({ prop }) => {
+
+  const [sayac, sayacGuncelle] = useState(0) // ⬅ bu kod sadece ilk renderda çalışır
+  const prevPropRef = useRef(prop) // ⬅ bu kod sadece ilk renderda çalışır
+
+  useEffect(() => {
+    console.log("Önceki prop:", prevPropRef.current);
+    console.log("Güncel prop:", prop);
+    
+    prevPropRef.current = prop
+  }, [prop]);
+
   return (
     <div>
-      App
+      <p>Prop: {prop}</p>
     </div>
   );
-}
+};
 
 export default App;
